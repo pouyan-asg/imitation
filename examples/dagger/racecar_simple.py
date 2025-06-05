1cf7b36a044c68946a29641d8424d21a69620471
pouyan-asg <pouyan.asgharian@yahoo.com>
2025-06-05 15:46:15 -0400

    CarRacing dagger pretrained

--- examples/dagger/racecar_simple.py
+++ examples/dagger/racecar_simple.py
@@ -1,6 +1,4 @@
-"""implementation of DAgger for CarRacing
-https://gymnasium.farama.org/environments/box2d/car_racing/
-"""
+"""implementation of a simple DAgger for CarRacing-v2 environment using a pre-trained expert policy."""
 
 import os
 import numpy as np
@@ -8,10 +6,14 @@ import gymnasium as gym
 from imitation.policies.serialize import load_policy
 from imitation.util.util import make_vec_env
 from imitation.algorithms import bc
+from imitation.data import rollout
 from imitation.algorithms.dagger import SimpleDAggerTrainer
+from imitation.data.wrappers import RolloutInfoWrapper
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.vec_env import DummyVecEnv, VecTransposeImage
 from datetime import datetime
+
+# more info: https://gymnasium.farama.org/environments/box2d/car_racing/
 
 # -------Initialize----------
 root_path = "/home/pouyan/phd/imitation_learning/imitation/examples/dagger/logs"
@@ -19,22 +21,28 @@ timestamp = datetime.now().strftime("%Y%m%d_%H%M")
 logs_dir = os.path.join(root_path, f"{timestamp}")
 
 rn_seed = np.random.default_rng(0)
-env_max_episode_steps = 500
-n_eval_episodes = 50
+train_min_episodes = 2
+train_min_timesteps = 10
+n_eval_episodes = 2
+training_epochs = 1
+
+sample_until = rollout.make_sample_until(
+    min_timesteps=train_min_timesteps,
+    min_episodes=train_min_episodes,)
 
 # -------Create a vectorized environment----------
 """since the policy's observation space is CHW format, we need to 
 transpose the our environemnt's space from HWC to CHW by VecTransposeImage"""
 
-env = VecTransposeImage(DummyVecEnv([lambda: gym.make("CarRacing-v2", 
-                                    render_mode="rgb_array",
+env = VecTransposeImage(DummyVecEnv([lambda: RolloutInfoWrapper(gym.make("CarRacing-v2", 
+                                    render_mode="human",
                                     lap_complete_percent=0.95,
                                     domain_randomize=False,
-                                    continuous=True)]))
+                                    continuous=True))]))
 
 env.seed(0)
 
-# -------Create expert trajectories from a pre-trained policy----------
+# -------- Create the expert policy---------
 expert = load_policy(
     policy_type="ppo-huggingface",
     organization="igpaub",
@@ -46,6 +54,10 @@ print(f"env.observation_space: {env.observation_space}")
 print(f"env.action_space: {env.action_space}")
 print(f"expert.observation_space: {expert.observation_space}")
 print(f"expert.action_space: {expert.action_space}")
+
+# -------quick evaluation of the expert policy----------
+# reward, _ = evaluate_policy(model=expert, env=env, n_eval_episodes=n_eval_episodes, deterministic=True)
+# print(f"Expert Reward ==> maximum:1000, my:{reward} \n")
 
 # -------Prepare expert and algorithm----------
 bc_trainer = bc.BC(
